ef9310ac9556794e5ec66b354b33dc84a3ebfb8e
cihanarslan <32307607+cihanarslan@users.noreply.github.com>
2018-04-09 17:44:09 +0300

    hw2

---
+++ action_page.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Antalya</title>	
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+  <link rel="icon" href="images/orange.png">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="home.htm"> <img src="images/orange.png" width="50" height="50"> ANTALYA </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="home.htm">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="localdishes.htm">Local Dishes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="historicalplaces.htm">Historical Places</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="usefullinks.htm">Useful Links</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aboutme.htm">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contactme.htm">Contact Me</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="registration.htm">Registration</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div align="center" style="margin-top: 10px; font-family: 'Amatic SC', cursive;">
+    <h3><b>Registration Successful - Thank You<b></h3>
+    <img src="images/check.png" width="400px" height="400px">
+    <a href="registration.htm">
+      <div style="background-color: #DCDCDC; margin-top: 20px;">
+        <img src="images/back.png" width="100px" height="100px">
+        <p style="font size: 17px">Go back</p>
+      </div>
+    </a>
+  </div>
+ 
+</body>
+</html>
+
+
